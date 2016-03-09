a7c3afc753f74e00c5d3fc3d32c15744d03f4784
sagars01 <sagarmoysengupta@gmail.com>
2016-03-09 20:40:15 +0530

    P

---
+++ cdn.php
@@ -0,0 +1,12 @@
+  <meta charset="utf-8">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>XD Studio</title>
+        <meta name="description" content="">
+        <meta name="author" content="Sagarmoy">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0">   
+        <link href="custom.css" rel="stylesheet" type="text/css" />
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css" rel="stylesheet"><!---bootstrap CSS-->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" type="text/javascript"></script><!---bootstrap--->
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script><!---Angular--->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> <!---jquery-->
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <?php include 'cdn.php'?>
+    </head>
+    <body>
+        
+        
+        
+    <script src="xdone.js"></script>    
+    </body>    
+</html>
